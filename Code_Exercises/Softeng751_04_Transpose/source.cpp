2ac725ff347e0713548bcb859c5e5c1c13466483
Julian Oppermann <julian.oppermann@codeplay.com>
2024-04-12 12:31:53 +0100

    Test.
    Signed-off-by: Julian Oppermann <julian.oppermann@codeplay.com>

--- Code_Exercises/Softeng751_04_Transpose/source.cpp
+++ Code_Exercises/Softeng751_04_Transpose/source.cpp
@@ -42,28 +42,31 @@ TEST_CASE("transpose", "transpose") {
     auto width = inputImage.width();
     auto height = inputImage.height();
     auto channels = inputImage.channels();
+    assert(channels == 4);
 
     auto bufferRange = range<3>(height, width, channels);
+    auto globalRange = range<2>(height, width);
 
     {
       auto inputBuf = buffer{inputImage.data(), bufferRange};
       auto outputBuf = buffer<float, 3>{bufferRange};
       outputBuf.set_final_data(outputImage.data());
 
+      auto inputBufVec = inputBuf.reinterpret<sycl::float4>(globalRange);
+      auto outputBufVec = outputBuf.reinterpret<sycl::float4>(globalRange);
+
       util::benchmark(
           [&]() {
             myQueue.submit([&](handler &cgh) {
-              accessor inputAcc{inputBuf, cgh, read_only};
-              accessor outputAcc{outputBuf, cgh, write_only};
+              accessor inputAcc{inputBufVec, cgh, read_only};
+              accessor outputAcc{outputBufVec, cgh, write_only};
 
-              cgh.parallel_for<transpose>(
-                  nd_range<2>(range<2>(height, width), range<2>(16, 16)),
+              cgh.parallel_for<transpose>(globalRange,
                   [=](id<2> idx) {
                     auto i = idx[0]; // row
                     auto j = idx[1]; // column
 
-                    for (int c = 0; c < 4; ++c)
-                      outputAcc[i][j][c] = inputAcc[j][i][c];
+                    outputAcc[i][j] = inputAcc[j][i];
                   });
             });
 
--- Code_Exercises/Softeng751_05_TiledTranspose/source.cpp
+++ Code_Exercises/Softeng751_05_TiledTranspose/source.cpp
@@ -42,39 +42,43 @@ TEST_CASE("tiled_transpose", "tiled_transpose") {
     auto width = inputImage.width();
     auto height = inputImage.height();
     auto channels = inputImage.channels();
+    assert(channels == 4);
 
     auto bufferRange = range<3>(height, width, channels);
+    auto globalRange = range<2>(height, width);
+    auto localRange = range<2>(16, 16);
+    auto ndRange = nd_range<2>(globalRange, localRange);
 
     {
       auto inputBuf = buffer{inputImage.data(), bufferRange};
       auto outputBuf = buffer<float, 3>{bufferRange};
       outputBuf.set_final_data(outputImage.data());
 
+      auto inputBufVec = inputBuf.reinterpret<sycl::float4>(globalRange);
+      auto outputBufVec = outputBuf.reinterpret<sycl::float4>(globalRange);
+
       util::benchmark(
           [&]() {
             myQueue.submit([&](handler &cgh) {
-              accessor inputAcc{inputBuf, cgh, read_only};
-              accessor outputAcc{outputBuf, cgh, write_only};
-              local_accessor<float, 3> tileAcc{range<3>(16, 16, 4), cgh};
+              accessor inputAcc{inputBufVec, cgh, read_only};
+              accessor outputAcc{outputBufVec, cgh, write_only};
+              // local_accessor<float4, 2> tileAcc{localRange, cgh};
 
-              cgh.parallel_for<transpose>(
-                  nd_range<2>(range<2>(height, width), range<2>(16, 16)),
+              cgh.parallel_for<transpose>(ndRange,
                   [=](nd_item<2> item) {
-                     auto gi = item.get_global_id(0); // global row
-                     auto gj = item.get_global_id(1); // global column
-                     auto li = item.get_local_id(0);  // local row
-                     auto lj = item.get_local_id(1);  // local column 
+                    auto gi = item.get_global_id(0); // global row
+                    auto gj = item.get_global_id(1); // global column
+                    auto li = item.get_local_id(0);  // local row
+                    auto lj = item.get_local_id(1);  // local column
+                    auto gp = item.get_group();      // current work-group 
+                    auto ls = localRange[0];         // work-group size
+                    
+                    // tileAcc[lj][li] = inputAcc[gi][gj];
 
-                    for (int c = 0; c < 4; ++c)
-                      tileAcc[lj][li][c] = inputAcc[gi][gj][c];
+                    // group_barrier(gp);
 
-                    group_barrier(item.get_group());
-
-                    auto base_i = item.get_group()[1] * 16;
-                    auto base_j = item.get_group()[0] * 16;
-
-                    for (int c = 0; c < 4; ++c)
-                      outputAcc[base_i + li][base_j + lj][c] = tileAcc[li][lj][c];
+                    outputAcc[gp[1] * ls + li][gp[0] * ls + lj]
+                        = inputAcc[gi][gj]; // tileAcc[li][lj];
                   });
             });
 
